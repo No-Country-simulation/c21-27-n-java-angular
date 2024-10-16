515f02363a069564f338d164f52abdd78ed3b729
ale94 <alejandrorua111@gmail.com>
2024-10-15 21:57:32 -0300

    feat(be): entidad transaction creada.

---
+++ backend/src/main/java/com/example/banca_online_c21/controllers/Transaction.java
@@ -0,0 +1,27 @@
+package com.example.banca_online_c21.controllers;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String destinationAccount;
+    private Double amount;
+    private String description;
+    private LocalDateTime date;
+}
