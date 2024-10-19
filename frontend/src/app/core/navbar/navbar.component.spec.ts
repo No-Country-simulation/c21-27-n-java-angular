736986422cfb9c1737577b89a842b38145bdaa1e
Nico R. Olmos <romero.nico18@gmail.com>
2024-10-19 04:33:16 -0300

    feat(fe): add nav icons

--- frontend/src/app/core/navbar/navbar.component.spec.ts
+++ frontend/src/app/core/navbar/navbar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavMobileComponent } from './nav-mobile.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('NavMobileComponent', () => {
-  let component: NavMobileComponent;
-  let fixture: ComponentFixture<NavMobileComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavMobileComponent]
+      imports: [NavbarComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(NavMobileComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
