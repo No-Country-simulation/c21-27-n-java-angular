d084e4f716fadf4eefdfad0d4788f3e30773e79d
Nico R. Olmos <romero.nico18@gmail.com>
2024-10-19 16:28:27 -0300

    feat: add color palettes to taliwind variants

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,10 +8,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "dark-green": "#17412D",
-        "medium-green": "#326D3C",
-        "bright-green": "#70D560",
-        "light-green": "#A9E6A0",
+        "ob-terciary-dark-green": "#17412D",
+        "ob-title-medium-green": "#326D3C",
+        "ob-main-bright-green": "#70D560",
+        "ob-secondary-light-green": "#A9E6A0",
+        "ob-danger-red": "#ED6868",
+        "ob-black": "#161616",
+        "ob-gray": "#BABABA",
+        "ob-white": "#FCFCFC",
       },
     },
   },
