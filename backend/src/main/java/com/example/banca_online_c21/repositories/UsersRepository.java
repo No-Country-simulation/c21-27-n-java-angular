992bdf8fbb96ba5d1402c3cc440d4b61d452d2e0
Andres A <ing.andres.avenda@gmail.com>
2024-10-18 21:00:10 -0400

    fix(be): Se modifica la Entidad User para generar numero de cuenta aleatorio cuando se crea un user

--- backend/src/main/java/com/example/banca_online_c21/repositories/UsersRepository.java
+++ backend/src/main/java/com/example/banca_online_c21/repositories/UsersRepository.java
@@ -2,8 +2,14 @@ package com.example.banca_online_c21.repositories;
 
 import com.example.banca_online_c21.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UsersRepository extends JpaRepository<Users, Integer> {
 
+    boolean existsByUsername(String username);
 
+    Optional<Users> findByUsername(String username);
 }
