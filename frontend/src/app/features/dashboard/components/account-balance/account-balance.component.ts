841886afa140b94900e789520a1f5f0648d29500
vantabytes <romero.nico18@gmail.com>
2024-10-20 17:58:06 -0300

    feat(fe): append tailwind variant basis

--- frontend/src/app/features/dashboard/components/account-balance/account-balance.component.ts
+++ frontend/src/app/features/dashboard/components/account-balance/account-balance.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { OB_BALANCE } from '../../../../styles/balance.tv';
 
 @Component({
   selector: 'app-account-balance',
   standalone: true,
   imports: [],
   templateUrl: './account-balance.component.html',
-  styleUrl: './account-balance.component.scss'
+  styleUrl: './account-balance.component.scss',
 })
 export class AccountBalanceComponent {
-
+  obBalance = OB_BALANCE;
 }
---
+++ frontend/src/app/styles/balance.tv.ts
@@ -0,0 +1,5 @@
+import { tv } from 'tailwind-variants';
+
+export const OB_BALANCE = tv({
+  base: 'text-4xl',
+});
