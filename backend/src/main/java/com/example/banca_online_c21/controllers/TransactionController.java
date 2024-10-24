8f7a2cfb4728303218610c59a441929fefeb5694
ale94 <alejandrorua111@gmail.com>
2024-10-23 21:45:48 -0300

    feat(be): agregar comprobante pdf.

--- backend/src/main/java/com/example/banca_online_c21/controllers/TransactionController.java
+++ backend/src/main/java/com/example/banca_online_c21/controllers/TransactionController.java
@@ -2,11 +2,14 @@ package com.example.banca_online_c21.controllers;
 
 import com.example.banca_online_c21.entities.TransactionEntity;
 import com.example.banca_online_c21.services.ITransactionService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -32,5 +35,10 @@ public class TransactionController {
         return ResponseEntity.noContent().build();
     }
 
+    @GetMapping("receipt/{operationNumber}/pdf")
+    void generatePdf(@PathVariable UUID operationNumber, HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        this.transactionService.generatePdf(operationNumber, response);
+    }
 
 }
--- backend/src/main/java/com/example/banca_online_c21/repositories/TransactionRepository.java
+++ backend/src/main/java/com/example/banca_online_c21/repositories/TransactionRepository.java
@@ -4,6 +4,12 @@ import com.example.banca_online_c21.entities.TransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
+
+    Optional<TransactionEntity> findByOperationNumber(UUID operationNumber);
+
 }
