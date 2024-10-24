2096c83e06e601a6beb1dfa56aa31022a1e64d92
vantabytes <romero.nico18@gmail.com>
2024-10-24 00:32:35 -0300

    feat(fe): add transfer card

---
+++ frontend/src/app/features/dashboard/components/recents/components/transfer-card/transfer-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transfer-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './transfer-card.component.html',
+  styleUrl: './transfer-card.component.scss'
+})
+export class TransferCardComponent {
+
+}
--- frontend/src/app/features/dashboard/components/recents/recents.component.ts
+++ frontend/src/app/features/dashboard/components/recents/recents.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { OB_PADDING } from '@styles/obBoxes.tv';
 import { OB_BUTTON_OUTLINED } from '@styles/obButtons.tv';
 import { OB_TEXT_HEADINGS } from '@styles/obTexts.tv';
+import { TransferCardComponent } from './components/transfer-card/transfer-card.component';
 
 @Component({
   selector: 'app-recents',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, TransferCardComponent],
   templateUrl: './recents.component.html',
   styleUrl: './recents.component.scss',
 })
