ad69562f0d8a15351f9da0672a5e9decd019fe4d
Andres A <ing.andres.avenda@gmail.com>
2024-10-25 16:50:25 -0400

    feat(be): Se realiza API de Transaction donde el usuario puede transferir el monto a una cuenta

--- backend/src/main/java/com/example/banca_online_c21/services/ITransactionService.java
+++ backend/src/main/java/com/example/banca_online_c21/services/ITransactionService.java
@@ -1,8 +1,10 @@
 package com.example.banca_online_c21.services;
 
+import com.example.banca_online_c21.DTO.TransferRequest;
 import com.example.banca_online_c21.entities.TransactionEntity;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.security.auth.login.AccountNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +19,7 @@ public interface ITransactionService {
     void delete(Long id);
 
     void generatePdf(UUID operationNumber, HttpServletResponse response) throws IOException;
+
+    void transferFunds(TransferRequest transferRequest) throws AccountNotFoundException; // Agrega esta línea
+
 }
