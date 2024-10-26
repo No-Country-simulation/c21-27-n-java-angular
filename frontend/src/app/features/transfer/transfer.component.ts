6b3c59d61552af96dca68c0613f2f18f9a390dfb
Nico R. Olmos <romero.nico18@gmail.com>
2024-10-26 14:30:40 -0300

    feat(fe): add transfer form validations with styles

--- frontend/src/app/features/transfer/transfer.component.ts
+++ frontend/src/app/features/transfer/transfer.component.ts
@@ -1,7 +1,13 @@
 import { NgxCurrencyDirective } from 'ngx-currency';
-import { CurrencyPipe, NgClass } from '@angular/common';
+
+import { CommonModule, CurrencyPipe, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { SubNavMobileComponent } from '@core/sub-nav-mobile/sub-nav-mobile.component';
 
 @Component({
@@ -13,20 +19,58 @@ import { SubNavMobileComponent } from '@core/sub-nav-mobile/sub-nav-mobile.compo
     SubNavMobileComponent,
     NgxCurrencyDirective,
     ReactiveFormsModule,
+    CommonModule,
   ],
   templateUrl: './transfer.component.html',
-  styleUrl: './transfer.component.scss',
+  styleUrls: ['./transfer.component.scss'],
 })
 export class TransferComponent {
-  transferForm = new FormGroup({
-    amount: new FormControl(''),
-    destination: new FormControl(''),
-    description: new FormControl(''),
-  });
+  transferForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.transferForm = this.formBuilder.group({
+      amount: [0, [Validators.required, Validators.min(100)]],
+      destination: ['', [Validators.required]],
+      description: ['', [Validators.maxLength(20)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.transferForm.valid) {
+      console.log(this.transferForm.value);
+    }
+  }
+
+  // Métodos para obtener mensajes de error
+  getErrorMessage(controlName: string): string | null {
+    const control = this.transferForm.get(controlName);
+
+    if (control?.hasError('required')) {
+      return 'Este campo es obligatorio';
+    }
+    if (control?.hasError('min')) {
+      return 'Monto mínimo $100,00';
+    }
+    if (control?.hasError('maxlength')) {
+      return 'Máximo 20 caracteres permitidos';
+    }
+    return null;
+  }
+
+  get amountCtl() {
+    return this.transferForm.get('amount');
+  }
+
+  get destinationCtl() {
+    return this.transferForm.get('destination');
+  }
+
+  get descriptionCtl() {
+    return this.transferForm.get('description');
+  }
 
   moveCursorToEnd(event: FocusEvent): void {
     const input = event.target as HTMLInputElement;
-    // Mover el cursor al final
     setTimeout(() => {
       input.setSelectionRange(input.value.length, input.value.length);
     }, 0);
