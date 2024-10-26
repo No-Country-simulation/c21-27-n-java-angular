8c119f9530405db6b752e73f7666dda7cd8e329f
Nico R. Olmos <romero.nico18@gmail.com>
2024-10-26 12:14:42 -0300

    feat(fe): add ngx currency dependency

--- frontend/src/app/features/transfer/transfer.component.ts
+++ frontend/src/app/features/transfer/transfer.component.ts
@@ -1,13 +1,34 @@
+import { NgxCurrencyDirective } from 'ngx-currency';
 import { CurrencyPipe, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-
-import { SubNavMobileComponent } from '../../core/sub-nav-mobile/sub-nav-mobile.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { SubNavMobileComponent } from '@core/sub-nav-mobile/sub-nav-mobile.component';
 
 @Component({
   selector: 'app-transfer',
   standalone: true,
-  imports: [CurrencyPipe, NgClass, SubNavMobileComponent],
+  imports: [
+    CurrencyPipe,
+    NgClass,
+    SubNavMobileComponent,
+    NgxCurrencyDirective,
+    ReactiveFormsModule,
+  ],
   templateUrl: './transfer.component.html',
   styleUrl: './transfer.component.scss',
 })
-export class TransferComponent {}
+export class TransferComponent {
+  transferForm = new FormGroup({
+    amount: new FormControl(''),
+    destination: new FormControl(''),
+    description: new FormControl(''),
+  });
+
+  moveCursorToEnd(event: FocusEvent): void {
+    const input = event.target as HTMLInputElement;
+    // Mover el cursor al final
+    setTimeout(() => {
+      input.setSelectionRange(input.value.length, input.value.length);
+    }, 0);
+  }
+}
