cefce4350da6df5f52fcce5791d0fba15eed46c7
ale94 <alejandrorua111@gmail.com>
2024-10-27 11:00:22 -0300

    feat(be): actualizar cors de authcontroller

--- backend/src/main/java/com/example/banca_online_c21/controllers/AuthController.java
+++ backend/src/main/java/com/example/banca_online_c21/controllers/AuthController.java
@@ -15,11 +15,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
