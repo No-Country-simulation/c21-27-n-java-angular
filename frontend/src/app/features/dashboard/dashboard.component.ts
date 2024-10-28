87f85f8cf74ebb30fb20e4b05313a7b373ec17e9
Nico R. Olmos <romero.nico18@gmail.com>
2024-10-28 16:29:38 -0300

    feat(fe): update dashboard header

--- frontend/src/app/features/dashboard/dashboard.component.ts
+++ frontend/src/app/features/dashboard/dashboard.component.ts
@@ -1,24 +1,22 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { BalanceService } from '@shared/services/balance.service';
-
-import { AccountBalanceComponent } from './components/account-balance/account-balance.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ShortcutsComponent } from './components/shortcuts/shortcuts.component';
-import { RecentsComponent } from './components/recents/recents.component';
-import { UserSesionComponent } from '../../core/user-sesion/user-sesion.component';
-import { NgClass } from '@angular/common';
 import { obPadding } from '@styles/ob-box-model.tv';
+
+import { UserSesionComponent } from '../../core/user-sesion/user-sesion.component';
+import { AccountBalanceComponent } from './components/account-balance/account-balance.component';
+import { RecentsComponent } from './components/recents/recents.component';
+import { ObButtonCircleComponent } from '@shared/components/ob-button-circle/ob-button-circle.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
     AccountBalanceComponent,
-    HeaderComponent,
-    ShortcutsComponent,
     RecentsComponent,
     UserSesionComponent,
     NgClass,
+    ObButtonCircleComponent,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
